cf151435b8af4d509022e8ad4d29d0e56147921a
fdobad <29801096+fdobad@users.noreply.github.com>
2024-01-17 19:21:28 -0300

    redaccion

--- 17-01/main.py
+++ 17-01/main.py
@@ -1,8 +1,11 @@
 import ctypes
+from platform import system
 
 # Cargamos la biblioteca compartida
-lib = ctypes.CDLL('./holamundo.so')  # En Linux/macOS
-# lib = ctypes.CDLL('./holamundo.dll')  # En Windows
+if system() == "Windows":
+    lib = ctypes.CDLL("./holamundo.dll")  # En Windows
+else:
+    lib = ctypes.CDLL("./holamundo.so")  # En Linux/macOS
 
 # Llamamos a la función hola_mundo desde la biblioteca
 lib.hola_mundo()
